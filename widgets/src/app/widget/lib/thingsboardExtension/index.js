713676c8c73d83803d2688f85c356ebf056ccca6
Dmitriymush <powder322@gmail.com>
2020-03-12 15:11:45 +0200

    1st fix

--- widgets/src/app/widget/lib/thingsboardExtension/index.js
+++ widgets/src/app/widget/lib/thingsboardExtension/index.js
@@ -3,9 +3,11 @@
  */
 import thingsboardExtensionTypes from './thingsboard-extension-types.constant';
 import thingsboardExtensionConfig from './thingsboard-extension-config';
+import primatechEntityTable from './custom-entity-table/entities-table-widget';
 
 export default angular.module('thingsboardExtension', [
-        thingsboardExtensionTypes
+        thingsboardExtensionTypes,
+        primatechEntityTable
     ])
     .config(thingsboardExtensionConfig)
     .name;
